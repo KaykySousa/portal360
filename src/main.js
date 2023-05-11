c1e2fad5a1f51296db2f3f1ca411f69f8a0e926b
KaykySousa <kaykysousa321@gmail.com>
2023-05-10 22:31:54 -0300

    feat: globe

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import "./style.scss"
+
+import Globe from "globe.gl"
+import countries from "./assets/globe/countries.json"
+import { getColor } from "./utils/colors"
+
+const globeContainer = document.querySelector("#globe-container")
+const world = Globe()
+
+world(globeContainer)
+	.globeImageUrl("./assets/globe/earth-day.jpg")
+	.polygonsData(countries.features)
+	.polygonCapColor(() => {
+		return getColor(100)
+	})
+	.backgroundColor("#0000")
