c5394b6d0a405e7227d738f6eae63b251ed3574c
Gustavo Santos Siqueira <gusiqueira2010@gmail.com>
2023-05-18 21:56:39 -0300

    feat: color scale based on range of data values

--- src/utils/gini.js
+++ src/utils/gini.js
@@ -1,6 +1,22 @@
 import gini from "../assets/data/gini.json"
+import { yearSelected } from "../main"
 import { getColor } from "./colors"
+import { percentageScale } from "./scale"
 
 export function getGiniColor(country) {
     const index = gini[country.properties.ISO_A3]
+
+    const sortededCountries = Object.values(gini)
+        .sort((a, b) => (a[yearSelected] > b[yearSelected] ? 1 : -1))
+        .filter((country) => country[yearSelected])
+
+    return getColor(
+        index
+            ? percentageScale(
+                  sortededCountries[0][yearSelected],
+                  sortededCountries[sortededCountries.length - 1][yearSelected],
+                  index[yearSelected]
+              )
+            : null
+    )
 }
