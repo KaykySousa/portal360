d0bc4de132675533d48c8d139a1ced5a3bac98f6
KaykySousa <kaykysousa321@gmail.com>
2023-05-20 10:51:47 -0300

    feat: index type select

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import "./style.scss"
 import { World } from "./world"
 
 const yearSelect = document.getElementById("year-select")
+const indexTypeSelect = document.getElementById("index-type-select")
 
-const world = new World()
+export const world = new World()
 
 yearSelect.addEventListener("input", (e) => world.updateYear(e.target.value))
+indexTypeSelect.addEventListener("input", (e) => world.updateIndexType(e.target.value))
