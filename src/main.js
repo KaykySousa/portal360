75989deab191bea28651a4a744ba98b278ad2bef
KaykySousa <kaykysousa321@gmail.com>
2023-06-06 12:15:52 -0300

    ref: move bootstrap modal ref to main file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "./style.scss"
+import { Modal } from "bootstrap"
 import * as bootstrap from "bootstrap"
 
 import { World } from "./world"
 
 export const world = new World()
+export const newsletterModal = new Modal("#newsletter-modal")
+export const dataModal = new Modal("#data-modal")
